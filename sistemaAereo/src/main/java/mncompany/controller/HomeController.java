d7e0fa81a5731ed64a109a0494e8b62c12229640
mgirlene <Mgirlene.silva0908@gmail.com>
2020-11-12 14:42:57 -0300

    Trazendo dados no banco na página de reservas

--- sistemaAereo/src/main/java/mncompany/controller/HomeController.java
+++ sistemaAereo/src/main/java/mncompany/controller/HomeController.java
@@ -1,14 +1,24 @@
 package mncompany.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import mncompany.domain.Voo;
+import mncompany.service.AssentoService;
+import mncompany.service.VooService;
 
 @Controller
 public class HomeController {
-
+	
+	@Autowired
+	private VooService service;
+	
+	@Autowired
+	private AssentoService serviceAssento;
+	
 	@GetMapping("/paginaPrincipal")
 	public String home(Voo voo) {
 		return "home";
@@ -20,8 +30,9 @@ public class HomeController {
 	}
 	
 	@PostMapping("/pesquisarPassagem")
-	public String pesquisarPassagem(Voo voo) {
-		//IMPLEMENTAR BUSCA
+	public String pesquisarPassagem(ModelMap model) {
+		model.addAttribute("voo", service.buscarTodos() );
+		model.addAttribute("assento", serviceAssento.buscarPorId(2L));
 		return "passagens";
 	}
 	
