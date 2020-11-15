31b90b5fc0aa07b2b3e0831c26e31f50c6f3215d
neudinho11 <neudinho66@gmail.com>
2020-11-15 16:54:22 -0300

    Modificações e query

--- sistemaAereo/src/main/java/mncompany/controller/UsuarioController.java
+++ sistemaAereo/src/main/java/mncompany/controller/UsuarioController.java
@@ -3,9 +3,9 @@ package mncompany.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import mncompany.domain.entity.Usuario;
@@ -23,23 +23,20 @@ public class UsuarioController {
 
 	}
 
-	@PostMapping("login")
+	@PostMapping("/login")
 	public ModelAndView entrar(Usuario usuario) {
 		Usuario user = usuarioService.buscarPorEmailESenha(usuario.getEmail(), usuario.getSenha());
 		if (user != null) {
-			return new ModelAndView("home/{" + user.getUuid() + "}");
+			return new ModelAndView("/pesquisarPassagem/{" + user.getUuid() + "}");
 		}
 
 		return new ModelAndView("redirect:/");
 	}
 
-	@PostMapping("cadastro")
-	public ModelAndView cadastrar(Usuario usuario, BindingResult result) {
-		if (result.hasErrors())
-			return entrar(usuario);
-
+	@PostMapping("/cadastro")
+	public ModelAndView cadastrar(@RequestBody Usuario usuario) {
 		usuarioService.salvar(usuario);
-		return new ModelAndView("home/{" + usuario.getUuid() + "}");
+		return new ModelAndView("/pesquisarPassagem/{" + usuario.getUuid() + "}");
 	}
 
 }
--- sistemaAereo/src/main/java/mncompany/controller/VooController.java
+++ sistemaAereo/src/main/java/mncompany/controller/VooController.java
@@ -1,8 +1,54 @@
 package mncompany.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
+import java.util.UUID;
 
-@RestController
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import mncompany.domain.entity.PesquisaVoo;
+import mncompany.service.VooService;
+
+@Controller
 public class VooController {
+	
+	private UUID id;
+	
+	@Autowired
+	private VooService vooService;
+	
+	
+	@GetMapping("/pesquisarPassagem/{id}")
+	public String pesquisa(PesquisaVoo voo, @PathVariable("id") UUID id) {
+		this.id = id;
+		return "home";
+	}
+	
+	@PostMapping("/pesquisarPassagem/{id}")
+	public ModelAndView buscar(@RequestBody PesquisaVoo voo, @PathVariable("id") UUID id) {
+		return new ModelAndView("/listagem/{" + id + "}/{" + voo.getOrigem() + "}/{" + voo.getDestino() + "}/{" + voo.getDataIda() 
+		+ "}/{" + voo.getDataVolta() + "}/{" + voo.getClasse() + "}");
+	}
+	
+	@GetMapping("/listagem/{id}/{origem}/{destino}/{dataIda}/{dataVolta}/{classe}")
+	public ModelAndView listagem(@PathVariable("id") UUID id, @PathVariable("origem") String origem, @PathVariable("destino") String destino,
+			@PathVariable("dataIda") LocalDate dataIda, @PathVariable("dataVolta") LocalDate dataVolta, @PathVariable("classe") String classe) {
+		
+		ModelAndView mv = new ModelAndView("/reservas/{id}");
+		
+		mv.addObject("vooIda", vooService.buscarPorVoos(origem, destino, dataIda));
+		
+		if(dataVolta != null) {
+			mv.addObject("vooVolta", vooService.buscarPorVoos(destino, origem, dataVolta));
+		}
+		
+		return mv;
+		
+	}
 
 }
