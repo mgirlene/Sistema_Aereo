817356302cb09e8ade949f5c9d714f62beb74b56
neudinho11 <neudinho66@gmail.com>
2020-12-24 18:01:14 -0300

    mudança de pagina

--- sistemaAereo/src/main/java/mncompany/controller/IndexController.java
+++ sistemaAereo/src/main/java/mncompany/controller/IndexController.java
@@ -4,12 +4,10 @@ package mncompany.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import mncompany.domain.entity.Passagem;
--- sistemaAereo/src/main/java/mncompany/controller/SituacaoPagamentoController.java
+++ sistemaAereo/src/main/java/mncompany/controller/SituacaoPagamentoController.java
@@ -1,6 +1,5 @@
 package mncompany.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
